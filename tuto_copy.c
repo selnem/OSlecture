56b6dce9d96a8ace6474a25e226ebff848130222
selnem <tjdghks0130@naver.com>
2026-03-12 08:21:49 +0000

    shupple

---
+++ tuto_copy.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 1024
+
+double A[N][N];
+double B[N][N];
+double C[N][N];
+
+int main(){
+	int i,j,k;
+	clock_t start, end;
+	double total_time;
+
+	//populate the matrices with random values between 0.0 and 1.0
+
+	for (i = 0; i < N; i++){
+		for(j = 0; j < N; j++){
+			A[i][j] = (double) rand() / (double) RAND_MAX;
+			B[i][j] = (double) rand() / (double) RAND_MAX;
+			C[i][j] = 0;
+		}
+	}
+
+	start = clock();
+	//matrix multipication
+	for(i = 0; i < N; i++){
+		for(k = 0;  k < N; k++){
+			for(j = 0; j < N; j++){
+				C[i][j] += A[i][k]*B[k][j];
+			}
+		}
+	}
+	end = clock();
+	total_time = (double)(end-start) / CLOCKS_PER_SEC;
+	printf("Elapsed time in seconds : %.3lf \n",total_time);
+}  

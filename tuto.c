a3acd537b7eb374060e4ab75f1e189a097bcc989
selnem <tjdghks0130@naver.com>
2026-03-17 04:45:58 +0000

    week2

--- tuto.c
+++ tuto.c
@@ -24,9 +24,9 @@ int main(){
 
 	start = clock();
 	//matrix multipication
-	for(i = 0; i < N; i++){
-		for(j = 0;  j < N; j++){
-			for(k = 0; k < N; k++){
+	for(k = 0; k < N; k++){
+		for(j = 0; j < N; j++){
+			for(i = 0; i < N; i++){
 				C[i][j] += A[i][k]*B[k][j];
 			}
 		}

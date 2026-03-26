eb16692d0be7fdbdf721106621605f5c6b7f6ee9
selnem <tjdghks0130@naver.com>
2026-03-26 17:25:19 +0900

    week4

---
+++ strace.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    char* str = "stracetest.txt";
+    FILE *pFile = NULL;
+    pFile = fopen( str, "w+" );
+
+    fprintf(pFile,"This is a test code.\n");
+    fflush(pFile);
+    fclose(pFile);
+}
